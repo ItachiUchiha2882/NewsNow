ec1392fba537844c5b60e0adeca1fbb5a7ac2e5f
Sourabh Bhosale <sbhosale28802@gmail.com>
2022-08-01 23:00:15 +0530

    After tut40
    minor issue fixed, moi nubness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     <>
       <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">NewsNow</Link>
+          <Link className="navbar-brand" onClick={() => { setTab('home'); localStorage.setItem('nav', 'home');}} to="/">NewsNow</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
